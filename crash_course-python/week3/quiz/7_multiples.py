045d96c26427bed6563f4ab9fe881300a4c64b4e
hendrixxD <maticsjnr@gmail.com>
2022-07-17 18:06:44 +0100

    multiples_of_7

---
+++ crash_course-python/week3/quiz/7_multiples.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Write a script that prints the multiples of 7 between 0 and 100. Print one multiple per line and avoid printing any numbers that aren't multiples of 7. Remember that 0 is also a multiple of 7.
+
+print()
+
+'''
+i = 0
+while i >= 0 and i <= 100:
+    print(i)
+    i += 7
+
