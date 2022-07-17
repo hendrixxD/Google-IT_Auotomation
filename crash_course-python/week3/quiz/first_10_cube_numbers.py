9498d6822868f4b02a4b46cd4be28931e554793d
hendrixxD <maticsjnr@gmail.com>
2022-07-17 18:09:00 +0100

    first_10_cube_numbers

---
+++ crash_course-python/week3/quiz/first_10_cube_numbers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Write a script that prints the first 10 cube numbers (x**3), starting with x=1 and ending with x=10.
+
+for __ in range(__,__):
+  print(__)
+'''
+
+for i in range(10): 
+  print(x**3)
